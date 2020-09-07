5f30a4a20cf78980bfa209240a03233b5abc17a8
GRFreire <gui.rugai.freire@gmail.com>
2020-09-06 22:01:25 -0300

    feat: add dark and light theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,14 @@
 const theme = {
-  colors: {
-    background: '#121214',
+  light: {
+    background: '#dad8da',
+    primary: '#8b3d3c',
+    secondary: '#2b2a2b',
+    text: '#e1e1e6'
+  },
+  dark: {
+    background: '#2b2a2b',
+    primary: '#8b3d3c',
+    secondary: '#dad8da',
     text: '#e1e1e6'
   }
 }
