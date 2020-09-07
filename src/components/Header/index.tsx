261881be611a460c0d9fcae19954579561c4d986
GRFreire <gui.rugai.freire@gmail.com>
2020-09-06 22:09:32 -0300

    feat: add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Container } from './styles'
+import Logo from '../Logo'
+
+interface IProps {
+  dark?: boolean
+}
+
+const Header: React.FC<IProps> = ({ dark }) => {
+  return (
+    <Container dark={dark}>
+      <Logo size={20} dark={dark} />
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  )
+}
+
+export default Header
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+interface IContainerProps {
+  dark?: boolean
+}
+
+export const Container = styled.div<IContainerProps>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+
+  width: 90%;
+  max-width: 980px;
+
+  margin: 0 auto;
+  margin-top: 15px;
+
+  background-color: ${props =>
+    props.dark ? props.theme.dark.background : props.theme.light.background};
+
+  nav ul {
+    display: flex;
+
+    li {
+      list-style: none;
+
+      a {
+        text-decoration: none;
+        color: ${props =>
+          props.dark
+            ? props.theme.light.background
+            : props.theme.dark.background};
+        text-transform: uppercase;
+        font-size: 1.2rem;
+        padding: 1.2rem;
+        transition: all 250ms linear 0s;
+
+        &:hover {
+          background: rgba(255, 255, 255, 0.15);
+        }
+      }
+    }
+  }
+`
