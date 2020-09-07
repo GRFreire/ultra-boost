6ae5766a9c8633f8d7d112e4ef1d37d71d3885ca
GRFreire <gui.rugai.freire@gmail.com>
2020-09-06 21:26:24 -0300

    feat: create next app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyle from '../styles/global'
+import theme from '../styles/theme'
+import Head from 'next/head'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default MyApp
