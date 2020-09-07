0771982e82eb3c4f4b3f6bb1a31b6d986a8302b2
GRFreire <gui.rugai.freire@gmail.com>
2020-09-07 01:43:48 -0300

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,38 @@
 import React from 'react'
 import Head from 'next/head'
 
-import { Container } from '../styles/pages/Home'
+import Shoes from '../assets/shoes.png'
+
+import { Container, Home as HomeContainer } from '../styles/pages/Home'
+import Header from '../components/Header'
 
 const Home: React.FC = () => {
   return (
     <Container>
       <Head>
-        <title>Homepage</title>
+        <title>Ultra Boost</title>
       </Head>
 
-      <h1>Home</h1>
+      <Header />
+
+      <HomeContainer>
+        <div className="left">
+          <h1>Run Faster</h1>
+          <h2>Made for high speed running</h2>
+
+          <div className="buttons">
+            <div className="main">
+              <a href="#shoe">See shoe</a>
+            </div>
+            <div>
+              <a href="#contact">Contact us</a>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <img src={Shoes} alt="Shoes image" />
+        </div>
+      </HomeContainer>
     </Container>
   )
 }
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -5,12 +5,83 @@ export const Container = styled.div`
   height: 100vh;
 
   display: flex;
-  justify-content: center;
-  align-items: center;
   flex-direction: column;
 
   h1 {
     font-size: 5.4rem;
-    color: ${props => props.theme.colors.text};
+    color: ${props => props.theme.light.primary};
+  }
+`
+export const Home = styled.div`
+  background-color: ${props => props.theme.light.background};
+  color: ${props => props.theme.light.primary};
+  font-size: 2rem;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  margin: 75px 200px 9vh 200px;
+
+  .left {
+    margin-right: 5%;
+    font-family: 'Russo One';
+    font-weight: 400;
+
+    h1 {
+      font-size: 4.2rem;
+      text-transform: uppercase;
+    }
+    h2 {
+      font-size: 3rem;
+      margin-top: 30px;
+    }
+
+    .buttons {
+      margin-top: 50px;
+      display: flex;
+      flex-direction: row;
+
+      div {
+        width: 200px;
+        height: 60px;
+        margin: 10px;
+
+        background-color: ${props => props.theme.light.background};
+        border: 3px solid ${props => props.theme.light.secondary};
+        border-radius: 4px;
+
+        font-size: 1.4rem;
+        font-family: 'Roboto';
+        font-weight: 600;
+        cursor: pointer;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        a {
+          color: ${props => props.theme.light.secondary};
+        }
+
+        &.main {
+          background-color: ${props => props.theme.light.primary};
+          border: none;
+
+          a {
+            color: ${props => props.theme.light.text};
+          }
+        }
+
+        a {
+          text-decoration: none;
+        }
+      }
+    }
+  }
+
+  .right img {
+    width: 500px;
   }
 `
